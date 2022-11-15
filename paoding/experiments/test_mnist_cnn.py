47ace1115d400fb92985181b53dac0c169b50eb7
Mark H Meng <mmhhss1991@gmail.com>
2022-11-15 16:39:41 +0800

    Modify demo code

--- paoding/experiments/test_mnist_cnn.py
+++ paoding/experiments/test_mnist_cnn.py
@@ -1,19 +1,10 @@
+import tensorflow as tf
+from tensorflow.keras import datasets
 
+import paoding.utility.training_from_data as training_from_data
+from paoding.evaluator import Evaluator
 from paoding.pruner import Pruner
 from paoding.sampler import Sampler
-from paoding.evaluator import Evaluator
-import os
-import shutil
-import time
-
-from tensorflow.keras import datasets, layers, models
-from tensorflow.keras.models import Sequential, Model
-from tensorflow.keras.layers import Dense, ReLU, Input, Dropout
-import tensorflow as tf
-import pandas as pd
-import numpy as np
-from tensorflow.keras import datasets
-import paoding.utility.training_from_data as training_from_data
 from paoding.utility.option import ModelType, SamplingMode
 
 # Hide GPU from visible devices
@@ -26,47 +17,35 @@ model_path = 'paoding/models/mnist_cnn'
 # The MNIST dataset contains 60,000 28x28 greyscale images of 10 digits.
 # There are 50000 training images and 10000 test images.
 
-(train_features, train_labels), (test_features,
-                                 test_labels) = datasets.mnist.load_data(path="mnist.npz")
-print("Training dataset size: ", train_features.shape, train_labels.shape)
-# Normalize pixel values to be between 0 and 1
-train_features, test_features = train_features / 255.0, test_features / 255.0
-
-print("Training dataset size: ", train_features.shape, train_labels.shape)
-
-optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
-
-(train_features, train_labels), (test_features,
-                                 test_labels) = datasets.mnist.load_data(path="mnist.npz")
+(train_features, train_labels), (test_features, test_labels) = datasets.mnist.load_data(path="mnist.npz")
 
 # Normalize pixel values to be between 0 and 1
-train_features = train_features.reshape(train_features.shape[0], 28, 28, 1) / 255.0,
-test_features = test_features.reshape(test_features.shape[0], 28, 28, 1) / 255.0
+train_features = train_features.reshape(
+    train_features.shape[0], 28, 28, 1) / 255.0,
+test_features = test_features.reshape(
+    test_features.shape[0], 28, 28, 1) / 255.0
 
 optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
-loss_fn=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 
-repeat=1
-round = 0
-while(round<repeat):
+training_from_data.train_mnist_cnn((train_features, train_labels),
+                                   (test_features, test_labels),
+                                   model_path,
+                                   overwrite=False,
+                                   use_relu=True,
+                                   optimizer_config=optimizer,
+                                   epochs=30)
 
-    training_from_data.train_mnist_cnn((train_features, train_labels),
-                                            (test_features, test_labels),
-                                            model_path,
-                                            overwrite=False,
-                                            use_relu=True,
-                                            optimizer_config=optimizer,
-                                            epochs=30)
+sampler = Sampler()
+sampler.set_strategy(mode=SamplingMode.IMPACT, params=(0.75, 0.25))
 
-    sampler = Sampler()
-    sampler.set_strategy(mode=SamplingMode.IMPACT, params=(0.75, 0.25))
+model_name = 'MNIST'
+target = 0.25
+step = 0.05
 
-    model_name = 'MNIST'
-    target = 0.5
-    step = 0.025
+evaluator = Evaluator(epsilons=[0.01, 0.05], batch_size=100)
 
-    evaluator = Evaluator(epsilons=[0.01, 0.05], batch_size=100)
-    pruner = Pruner(model_path,
+pruner = Pruner(model_path,
                 (test_features, test_labels),
                 target=target,
                 step=step,
@@ -75,13 +54,11 @@ while(round<repeat):
                 stepwise_cnn_pruning=True,
                 seed_val=42)
 
-    pruner.load_model(optimizer=optimizer, loss=loss_fn)
+pruner.load_model(optimizer=optimizer, loss=loss_fn)
+pruned_model_path = model_path + "_pruned"
+pruner.prune(evaluator=evaluator, pruned_model_path=pruned_model_path,
+             model_name=model_name, save_file=True)
 
-    #pruner.evaluate(verbose=1)
-    pruned_model_path = model_path + "_pruned"
-    pruner.prune(evaluator=evaluator, pruned_model_path=pruned_model_path, model_name=model_name, save_file=True)
+pruner.evaluate()
 
-    #pruner.evaluate(verbose=1)
-    pruner.quantization()
-    pruner.gc()
-    round += 1
+# END OF THE CODE
